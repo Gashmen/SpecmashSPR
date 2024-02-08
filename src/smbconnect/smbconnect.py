e5249bcfaf6e907c06ca2730a9ba7c4e9dd8e0d2
Gashmen <gashmen1234@gmail.com>
2024-02-08 09:46:49 +0300

    Создание возможности авторизации с домашнего хоста

--- src/smbconnect/smbconnect.py
+++ src/smbconnect/smbconnect.py
@@ -18,43 +18,53 @@ class SMBCONNECT_SPECMASH_SERVER:
         self.server_ip = smb_config.SERVER_IP
 
     def install_connect(self):
-        self.conn = SMBConnection(username=self.userID,
-                                  password=self.password,
-                                  my_name=self.client_machine_name,
-                                  remote_name=self.remote_machine_name,
-                                  domain=smb_config.DOMAIN,
-                                  use_ntlm_v2 = True)
+        if os.getlogin() != 'Gashmen':
+            self.conn = SMBConnection(username=self.userID,
+                                      password=self.password,
+                                      my_name=self.client_machine_name,
+                                      remote_name=self.remote_machine_name,
+                                      domain=smb_config.DOMAIN,
+                                      use_ntlm_v2 = True)
 
-        self.connection_status = self.conn.connect(self.server_ip, smb_config.PORT)#True or False
+            self.connection_status = self.conn.connect(self.server_ip, smb_config.PORT)#True or False
 
     def get_shell_csv_path(self):
-        if self.connection_status:#Если установлена коннект с сервером
-            self.file_obj_SHELL_CSV = tempfile.NamedTemporaryFile(delete=False)
-            self.shell_csv_path = self.file_obj_SHELL_CSV.name
-            filelist = self.conn.listShares(timeout=30)
+        if os.getlogin() != 'Gashmen':
+            if self.connection_status:#Если установлена коннект с сервером
+                self.file_obj_SHELL_CSV = tempfile.NamedTemporaryFile(delete=False)
+                self.shell_csv_path = self.file_obj_SHELL_CSV.name
+                filelist = self.conn.listShares(timeout=30)
 
-            file_attributes, filesize = self.conn.retrieveFile\
-                ('Docs',smb_config.SHELL_BASE_PATH,self.file_obj_SHELL_CSV)
-            self.file_obj_SHELL_CSV.close()
+                file_attributes, filesize = self.conn.retrieveFile\
+                    ('Docs',smb_config.SHELL_BASE_PATH,self.file_obj_SHELL_CSV)
+                self.file_obj_SHELL_CSV.close()
+        else:
+            self.shell_csv_path = 'D:\\Работа\\SpecmashSPR\\information_for_testing\\shell.csv'
 
     def get_gland_csv_path(self):
-        if self.connection_status:#Если установлена коннект с сервером
-            self.file_obj_GLAND_CSV = tempfile.NamedTemporaryFile(delete=False)
-            self.gland_csv_path = self.file_obj_GLAND_CSV.name
-            filelist = self.conn.listShares(timeout=30)
+        if os.getlogin() != 'Gashmen':
+            if self.connection_status:#Если установлена коннект с сервером
+                self.file_obj_GLAND_CSV = tempfile.NamedTemporaryFile(delete=False)
+                self.gland_csv_path = self.file_obj_GLAND_CSV.name
+                filelist = self.conn.listShares(timeout=30)
 
-            file_attributes, filesize = self.conn.retrieveFile\
-                ('Docs',smb_config.GLAND_BASE_PATH,self.file_obj_GLAND_CSV)
-            self.file_obj_GLAND_CSV.close()
+                file_attributes, filesize = self.conn.retrieveFile\
+                    ('Docs',smb_config.GLAND_BASE_PATH,self.file_obj_GLAND_CSV)
+                self.file_obj_GLAND_CSV.close()
+        else:
+            self.gland_csv_path = 'D:\\Работа\\SpecmashSPR\\information_for_testing\\Кабельные вводы ВЗОР(САПР).csv'
 
     def get_base_dxf_path(self):
-        if self.connection_status:#Если установлена коннект с сервером
-            self.file_obj_BASE_DXF = tempfile.NamedTemporaryFile(delete=False)
-            self.dxf_base_path = self.file_obj_BASE_DXF.name
-            filelist = self.conn.listShares(timeout=30)
+        if os.getlogin() != 'Gashmen':
+            if self.connection_status:#Если установлена коннект с сервером
+                self.file_obj_BASE_DXF = tempfile.NamedTemporaryFile(delete=False)
+                self.dxf_base_path = self.file_obj_BASE_DXF.name
+                filelist = self.conn.listShares(timeout=30)
 
-            file_attributes, filesize = self.conn.retrieveFile\
-                ('Docs',smb_config.DXF_BASE_PATH,self.file_obj_BASE_DXF)
-            self.file_obj_BASE_DXF.close()
+                file_attributes, filesize = self.conn.retrieveFile\
+                    ('Docs',smb_config.DXF_BASE_PATH,self.file_obj_BASE_DXF)
+                self.file_obj_BASE_DXF.close()
+        else:
+            self.dxf_base_path = 'D:\\Работа\\SpecmashSPR\\information_for_testing\\DXF_BASE.dxf'
 
 
