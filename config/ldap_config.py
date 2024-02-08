5fdaa2103f62b84bf035b069ed8818d2dda97b15
g.zubkov <gashmen1234@gmail.com>
2024-02-08 08:12:02 +0300

    Добавление конфиг файлов

---
+++ config/ldap_config.py
@@ -0,0 +1,13 @@
+domain = 'ZAVOD.ru'
+base = 'DC=ZAVOD,DC=ru'
+filter = "(&(objectcategory=person))"
+attrs = ['sAMAccountName']
+
+server_adres = '192.168.200.101'
+port = 389
+
+LOGIN = 'ZAVOD\g.zubkov'
+PASSWORD = 'QWErty!!11'
+
+
+
---
+++ config/smb_config.py
@@ -0,0 +1,11 @@
+USERLOGIN = "g.zubkov"
+PASSWORD = "QWErty!!11"
+# self.client_machine_name = "SM_PC_21"  == socket.gethostname()
+REMOTE_MACHINE_NAME = "fs_spb"
+SERVER_IP = "192.168.100.45"
+PORT = 139
+DOMAIN='ZAVOD.RU'
+
+SHELL_BASE_PATH = 'КД\\ВЗОР\\03 КД\\10 Светильники\\0. База КОМПАС\\3D модели\_Документация\\36. Библиотеки для ПО\\Скрипты Python\\САПР\\Оболочки\\shell.csv'
+GLAND_BASE_PATH = 'КД\\ВЗОР\\03 КД\\10 Светильники\\0. База КОМПАС\\3D модели\_Документация\\34. Разработки\\Номенклатура кабельных вводов\\Кабельные вводы ВЗОР(САПР).csv'
+DXF_BASE_PATH = 'КД\\ВЗОР\\03 КД\\10 Светильники\\0. База КОМПАС\\3D модели\_Документация\\36. Библиотеки для ПО\\Скрипты Python\\САПР\\dxf_base\\DXF_BASE.dxf'
