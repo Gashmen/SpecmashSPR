9ae2a526100e79bcdc2331c794269de3fd5965c1
Gashmen <gashmen1234@gmail.com>
2024-02-12 18:29:29 +0300

    Создание общего класса при открытие ошибки

---
+++ src/Widgets_Custom/UI_BaseError.py
@@ -0,0 +1,24 @@
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QDialog
+import sys
+
+class Ui_BaseError(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Error Window')
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.ShellError_label = QtWidgets.QLabel()
+        self.ShellError_label.setText('НЕТ ДАННОЙ ОБОЛОЧКИ В БАЗЕ DXF')
+        self.horizontalLayout.addWidget(self.ShellError_label)
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+
+    def call_shellerror(self):
+        self.show()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui_BaseError()
+    window.call_shellerror()
+    sys.exit(app.exec_())
